dc208cff5ce522b8ddc808e15736510f6af11f3b
sathish-dhana <cosmiccoincedenceinvoid@gmail.com>
2022-03-21 11:25:11 +0530

    done

--- Evaluvation-2 (mar 21)/src/com/masai/question3/Employee.java
+++ Evaluvation-2 (mar 21)/src/com/masai/question3/Employee.java
@@ -1,6 +1,65 @@
 package com.masai.question3;
 
-public class Employee {
-//    private int empID;
-//    private String
+public class Employee implements Comparable<Employee> {
+    private int empID;
+    private String name;
+    private String adress;
+    private Double salary;
+
+    public Employee() {
+    }
+
+    public Employee(int empID, String name, String adress, Double salary) {
+        this.empID = empID;
+        this.name = name;
+        this.adress = adress;
+        this.salary = salary;
+    }
+
+    public int getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+        if (this.salary < o.getSalary())
+            return 1;
+        else if (this.salary > o.getSalary())
+            return -1;
+        else
+            return name.compareTo(o.getName());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return empID + " " + name + " " + adress + " " + salary;
+    }
 }
--- Evaluvation-2 (mar 21)/src/com/masai/question3/Main.java
+++ Evaluvation-2 (mar 21)/src/com/masai/question3/Main.java
@@ -1,5 +1,32 @@
 package com.masai.question3;
 
-public class Main {
+import java.util.Scanner;
+import java.util.TreeSet;
 
+public class Main {
+    public static void main(String[] args) {
+        TreeSet<Employee> tree = new TreeSet<>();
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            System.out.println("Enter employee id, name, address, salary");
+            int id = sc.nextInt();
+            String Name = sc.next();
+            String address = sc.next();
+            double sal = sc.nextDouble();
+
+            Employee emp = new Employee(id, Name, address, sal);
+            tree.add(emp);
+
+            System.out.println("do you want to continue? (y/n)");
+
+            String check = sc.next();
+            if (check.equalsIgnoreCase("n")) {
+                break;
+            }
+        }
+
+        for (Employee x : tree) {
+            System.out.println(x.toString());
+        }
+    }
 }
